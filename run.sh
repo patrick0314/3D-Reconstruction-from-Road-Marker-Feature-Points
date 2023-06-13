36bd79146be070e34e591c6a6159949c8508ebad
patrick0314 <patrick0011223344@gmail.com>
2023-06-13 15:41:37 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-python src/main.py --seq seq1 --ICP_threshold 0.47
-python src/main.py --seq seq2 --ICP_threshold 0.47
-python src/main.py --seq seq3 --ICP_threshold 0.47
+#python src/main.py --seq seq1 --ICP_threshold 0.47
+#python src/main.py --seq seq2 --ICP_threshold 0.47
+#python src/main.py --seq seq3 --ICP_threshold 0.47
 
-#python src/main.py --seq test1 --ICP_threshold 0.47
-#python src/main.py --seq test2 --ICP_threshold 0.47
+python src/main.py --seq test1 --ICP_threshold 0.47
+python src/main.py --seq test2 --ICP_threshold 0.47
